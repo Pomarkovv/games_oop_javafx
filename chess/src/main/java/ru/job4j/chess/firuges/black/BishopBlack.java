fe49047d01a0d0c69d1e352e80b143b7c2179d51
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-07-01 11:05:33 +0700

    fix in chess

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -38,10 +38,7 @@ public class BishopBlack implements Figure {
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        if (Math.abs(source.getY() - dest.getY()) == Math.abs(source.getX() - dest.getX())) {
-            return true;
-        }
-        return false;
+        return (Math.abs(source.getY() - dest.getY()) == Math.abs(source.getX() - dest.getX()));
     }
 
     @Override
